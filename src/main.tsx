58ca9eca6fa5230107d387f721c31bf5fd2620ff
David Nogueira <davidsergiossa@gmail.com>
2023-12-13 14:12:12 -0300

    react-dom

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,25 @@ import './index.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import App from './App.tsx';
+import LoginScreen from './modules/login';
+
+//import App from './App.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: '/login',
+    element: <LoginScreen />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
