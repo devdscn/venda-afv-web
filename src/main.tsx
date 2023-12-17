7dcf954864aeecf9c19c3769ae926942b220e260
David Nogueira <davidsergiossa@gmail.com>
2023-12-17 20:41:17 -0300

    ajustes main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,29 +1,15 @@
-import './index.css';
+
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import LoginScreen from './modules/login';
+import App from './App';
 import { GlobalProvider } from './shared/hooks/useGlobalContext';
-
-//import App from './App.tsx';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <div>Hello world!</div>,
-  },
-  {
-    path: '/login',
-    element: <LoginScreen />,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <GlobalProvider>
-      <RouterProvider router={router} />
+      <App />
     </GlobalProvider>
   </React.StrictMode>,
 );
