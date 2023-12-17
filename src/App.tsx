d9477d5816f27041b22693446b08d10c7a2238a7
David Nogueira <davidsergiossa@gmail.com>
2023-12-17 20:40:44 -0300

    ajustes app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,41 @@
-import LoginScreen from './modules/login';
+import './index.css';
 
+import type { Router as RemixRouter } from '@remix-run/router';
+import {
+  createBrowserRouter,
+  RouteObject,
+  RouterProvider,
+} from 'react-router-dom';
+
+//import LoginScreen from './modules/login';
+import { loginRoutes } from './modules/login/routes';
+
+const mainRoutes: RouteObject[] = [
+  {
+    path: '/',
+    element: <div>Tela Principal</div>,
+    errorElement: <div>Página não encontrada</div>,
+  },
+];
+
+const router: RemixRouter = createBrowserRouter([
+  ...mainRoutes,
+  ...loginRoutes,
+]); /*
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: '/login',
+    element: <LoginScreen />,
+  },
+]);
+*/
 function App() {
-  return <LoginScreen />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
