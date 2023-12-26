ab1b430308cae7b108d900c4c3a6c4728308b3a6
David S C Nogueira <davidsergiossa@gmail.com>
2023-12-26 17:03:50 -0300

    feat: cores tabela produtos

--- src/modules/produto/screens/Produto.tsx
+++ src/modules/produto/screens/Produto.tsx
@@ -1,6 +1,7 @@
 import Table, { ColumnsType } from 'antd/es/table';
 import { useEffect } from 'react';
 
+import GrupoColuna from '../../../shared/components/GrupoColuna';
 import { URL_PRODUTO_EMPRESA } from '../../../shared/constants/urls';
 import { MethodsEnum } from '../../../shared/enums/methods.enum';
 import { useDataContext } from '../../../shared/hooks/useDataContext';
@@ -18,17 +19,23 @@ const columns: ColumnsType<ProdutoTypes> = [
     title: 'Id Empresa',
     dataIndex: 'idEmpresa',
     key: 'idEmpresa',
-    render: (text) => <a>{text}</a>,
   },
   {
     title: 'Nome',
     dataIndex: 'nome',
     key: 'nome',
+    render: (text) => <a>{text}</a>,
   },
   {
     title: 'Preço',
     dataIndex: 'preco',
     key: 'preco',
+  },
+  {
+    title: 'Grupo',
+    dataIndex: 'nomeGrupo',
+    key: 'nomeGrupo',
+    render: (_, produto) => <GrupoColuna nomeGrupo={produto} />,
   },
 ];
 
---
+++ src/shared/components/GrupoColuna.tsx
@@ -0,0 +1,31 @@
+import { Tag } from 'antd';
+
+import { ProdutoTypes } from '../../modules/produto/types/ProdutoTypes';
+
+interface GrupoColunaProps {
+  nomeGrupo?: ProdutoTypes;
+}
+
+const colors: string[] = [
+  'green',
+  'red',
+  'orange',
+  'gold',
+  'volcano',
+  'cyan',
+  'magenta',
+  'blue',
+  'geekblue',
+  'purple',
+  'lime',
+];
+
+const GrupoColuna = ({ nomeGrupo }: GrupoColunaProps) => {
+  if (!nomeGrupo) return null;
+
+  const currentColor = colors[nomeGrupo.nomeGrupo.length - 1] || colors[0];
+
+  return <Tag color={currentColor}>{nomeGrupo.nomeGrupo}</Tag>;
+};
+
+export default GrupoColuna;
