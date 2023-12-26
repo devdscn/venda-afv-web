969e46b2314d9837c711ef166b3b26305d3f31c8
David S C Nogueira <davidsergiossa@gmail.com>
2023-12-26 12:19:04 -0300

    feat: tabela de produtos

--- src/modules/produto/screens/Produto.tsx
+++ src/modules/produto/screens/Produto.tsx
@@ -1,3 +1,4 @@
+import Table, { ColumnsType } from 'antd/es/table';
 import { useEffect } from 'react';
 
 import { URL_PRODUTO_EMPRESA } from '../../../shared/constants/urls';
@@ -5,6 +6,31 @@ import { MethodsEnum } from '../../../shared/enums/methods.enum';
 import { useDataContext } from '../../../shared/hooks/useDataContext';
 import { useRequests } from '../../../shared/hooks/useRequests';
 import { ProdutoTypes } from '../types/ProdutoTypes';
+
+const columns: ColumnsType<ProdutoTypes> = [
+  {
+    title: 'Id',
+    dataIndex: 'idProduto',
+    key: 'idProduto',
+    render: (text) => <a>{text}</a>,
+  },
+  {
+    title: 'Id Empresa',
+    dataIndex: 'idEmpresa',
+    key: 'idEmpresa',
+    render: (text) => <a>{text}</a>,
+  },
+  {
+    title: 'Nome',
+    dataIndex: 'nome',
+    key: 'nome',
+  },
+  {
+    title: 'Preço',
+    dataIndex: 'preco',
+    key: 'preco',
+  },
+];
 
 const Produto = () => {
   const { produtos, setProdutos } = useDataContext();
@@ -18,8 +44,6 @@ const Produto = () => {
     );
   }, []);
 
-  return produtos.map((produto) => (
-    <div key={produto.id}>{`${produto.idProduto} | ${produto.nome}`}</div>
-  ));
+  return <Table columns={columns} dataSource={produtos} />;
 };
 export default Produto;
